53a0b1f89d7fca6f9b645a243e8e1607e425daa4
srusti-d <srustidonapati@gmail.com>
2026-02-28 20:07:04 -0600

    add updated caption for interactive plot.

--- pages/2_Explore.py
+++ pages/2_Explore.py
@@ -16,7 +16,7 @@ st.write("- Brush a specific year—do extremes cluster in particular periods?")
 st.write("- Compare histogram shape across weather types—what changes most: center, spread, or tails?")
 
 st.header("Exercise 7: Extreme daily temperature differences by weather type")
-st.write("We wish to see whether, for each weather type, do more extreme temperature differences correlate with levels of wind and precipitation?")
+st.write("We wish to see whether, for each weather type, do more extreme temperature differences correlate with levels of wind and precipitation? Select a specific time range and a weather type to examine corresponding wind and precipitation differences.")
 st.altair_chart(chart_temp_diff_wind(df), use_container_width=True)
 st.caption("Takeaway: Across weather types, there is primarily low precipitation (with the exception of rain and drizzle, of course). " \
 "Wind speeds are generally low to moderate. However, wind speeds increase in the later months of a year, more evident under snowy and rainy conditions.")
